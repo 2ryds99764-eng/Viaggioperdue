34bd5f12b9355fed89a9f91ec7c3745176ae6a1d
2ryds99764-eng <2ryds99764@privaterelay.appleid.com>
2026-06-17 07:52:28 +0200

    Fix hero e payoff

--- dati.js
+++ dati.js
@@ -54,7 +54,7 @@ const T42 = {
     /* ---- Stile immersivo della home (ispirato a Domitur) ---- */
     hero: "https://res.cloudinary.com/dspgc41gt/image/upload/Hero.jpg_ffkmdy",                   /* grande foto a tutto schermo */
     heroTitolo: "Viaggioperdue",
-    heroSotto: "Esistono luoghi dove il tempo sembra essersi fermato.",
+    heroSotto: "",
 
     /* le tre sezioni a parola singola che si incontrano scorrendo */
     sezioni: [
