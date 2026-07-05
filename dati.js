216c83eb3b542b2834c7b1a3e5c4b6d3493da0c3
2ryds99764-eng <lucianopisanelli@me.com>
2026-07-06 00:19:50 +0200

    Remove 'prestoOnline' property from dati.js
    Removed the 'prestoOnline' property from the object.

--- dati.js
+++ dati.js
@@ -799,7 +799,7 @@ const T42 = {
         mappa: "Azienda Agricola Pietre Levate, Località Bocito, Ottati SA",
         lat: 40.3760, lng: 15.2480,   /* Ottati (SA) — approssimativa, da affinare */
         storia: "pietrelevate",
-      prestoOnline: true,
+     
         galleria: []
       }
     ]
