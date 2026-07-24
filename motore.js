912ef57a3953ff90055bb34ebb3f081d106be0a3
2ryds99764-eng <lucianopisanelli@me.com>
2026-07-24 23:52:45 +0200

    Update motore.js
    Fix testo link hotel in home

--- motore.js
+++ motore.js
@@ -136,7 +136,7 @@ function costruisciHome() {
     invH.innerHTML =
       '<a class="racconto-invito invito-guida anima d3" href="hotel.html">' +
         '<span class="ri-occhiello">Gli alberghi</span>' +
-        '<span class="ri-titolo">Cerca tra gli alberghi per nome o provincia</span>' +
+        '<span class="ri-titolo">Cerca tra gli alberghi per nome o regione</span>' +
         '<span class="ri-azione">Cerca →</span>' +
       '</a>';
   }
